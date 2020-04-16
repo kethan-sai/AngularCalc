d240f92e8d84c3b7d8615474533a08c252453eea
kethan-sai <ykethansai@gmail.com>
2020-04-15 22:38:52 -0500

    intialCal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calculator';
+ 
+  MainText="";
+  opset = false;
+  allClear(){
+    
+    this.MainText="";
+  }
+
+  bcksp(){
+    let z: number =this.MainText.length - 1;
+    
+    this.MainText = this.MainText.substring(0,z);
+  
+  }
+  pressKey(key: string){
+    
+   
+    if(key === '/' || key === '+' || key === '-' || key=== '*'){
+      
+      const lastkey = this.MainText[this.MainText.length - 1];
+      if(lastkey === '/' || lastkey==='+' || lastkey==='-' || lastkey==='*'){
+        let z: number =this.MainText.length - 1;
+    
+        this.MainText = this.MainText.substring(0,z);
+      
+      }
+      if(this.MainText === ''){
+        return;
+      }
+      
+    }
+    
+   
+this.MainText +=key;
+
+  }
+
+  getres(){
+    this.MainText = eval(this.MainText);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule} from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +17,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
